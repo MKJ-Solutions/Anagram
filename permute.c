8e087f62ab06b02578ea0ed4113743010954fdf5
Miles Tjandrawidjaja <tjandrma@mcmaster.ca>
2012-10-18 14:03:40 -0400

    Permutations are now successfully working with no duplicates

--- permute.c
+++ permute.c
@@ -8,7 +8,7 @@ void RecursivePermute(char *prefix, char *rest, int *ptr);
 
 main()
 {
-    char letters[] = "AABC";
+    char letters[] = "AAB";
     ListPermutations(letters);
 
     return 0;
@@ -38,8 +38,14 @@ void ListPermutations(char *letters)
 
 void RecursivePermute (char *prefix, char *rest, int *ptr)
 {
+    //printf("--------------\n");
+    //printf("Prefix: %s\n", prefix);
+    //printf("Rest  : %s\n", rest);
     char *temp = malloc(sizeof(char *));
     char *new_prefix = malloc(sizeof(char *));
+    char *new_rest = malloc(sizeof(char *));
+    char *new_rest2 = malloc(sizeof(char *));
+    char *new_rest3 = malloc(sizeof(char *));
     char first_char;
     int idx = 0;
     int first_occurance = 0;
@@ -48,6 +54,8 @@ void RecursivePermute (char *prefix, char *rest, int *ptr)
     {
         *ptr += 1;
         printf(" Permutation %d: %s\n", *ptr, prefix);
+        printf("--------------\n");
+        return;
     }
     else
     {
@@ -61,12 +69,19 @@ void RecursivePermute (char *prefix, char *rest, int *ptr)
             {
                 //new_prefix = strcat(prefix, rest);
                 first_char = *rest;
+                //printf("INDEX: %d\n", idx);
+                //printf("Rest Size: %d\n", rest_size);
+                new_rest = strncpy(new_rest, rest-idx, idx);
+                new_rest2 = strncpy(new_rest2, rest+1, rest_size-1);
+                //printf("NEW_BETA: %s|%s\n", new_rest,new_rest2);
+                snprintf(new_rest3, sizeof new_rest3, "%s%s", new_rest, new_rest2);
+                //printf("NEW_REST: %s\n", new_rest3);
                 snprintf(new_prefix, sizeof new_prefix, "%s%s", prefix, &first_char);
+                //printf("NEW_PREF: %s\n", new_prefix);
                 //printf("Prefix: %s\n", prefix);
                 //printf("Rest : %c\n", first_char);
                 //printf("AM i working? %s\n", new_prefix);
-                RecursivePermute( new_prefix, ++rest, ptr);
-                --rest;
+                RecursivePermute( new_prefix, new_rest3, ptr);
             }
             rest++;
             idx ++;
