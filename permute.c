2bd5b4a40555b9414acbc8d14bc27514c94ff3ef
Miles Tjandrawidjaja <tjandrma@mcmaster.ca>
2012-10-18 14:18:49 -0400

    Variable renaming/cleanup

--- permute.c
+++ permute.c
@@ -4,7 +4,6 @@
 
 void ListPermutations(char *letters);
 void RecursivePermute(char *prefix, char *rest, int *ptr);
-//int  Strlen(char *word);
 
 main()
 {
@@ -38,28 +37,23 @@ void ListPermutations(char *letters)
 
 void RecursivePermute (char *prefix, char *rest, int *ptr)
 {
-    //printf("--------------\n");
-    //printf("Prefix: %s\n", prefix);
-    //printf("Rest  : %s\n", rest);
     char *temp = malloc(sizeof(char *));
     char *new_prefix = malloc(sizeof(char *));
+    char *rest_left = malloc(sizeof(char *));
+    char *rest_right = malloc(sizeof(char *));
     char *new_rest = malloc(sizeof(char *));
-    char *new_rest2 = malloc(sizeof(char *));
-    char *new_rest3 = malloc(sizeof(char *));
-    char first_char;
+    char rest_char;
     int idx = 0;
     int first_occurance = 0;
     strcpy(temp, rest);
     if (*rest == '\0')
     {
         *ptr += 1;
-        printf(" Permutation %d: %s\n", *ptr, prefix);
-        printf("--------------\n");
+        printf("Permutation %d: %s\n", *ptr, prefix);
         return;
     }
     else
     {
-        int i;
         size_t rest_size = strlen(rest);
         while (*rest != '\0')
         {
@@ -67,42 +61,15 @@ void RecursivePermute (char *prefix, char *rest, int *ptr)
             first_occurance = (strchr(temp, *rest) - temp - idx);
             if (!first_occurance)
             {
-                //new_prefix = strcat(prefix, rest);
-                first_char = *rest;
-                //printf("INDEX: %d\n", idx);
-                //printf("Rest Size: %d\n", rest_size);
-                new_rest = strncpy(new_rest, rest-idx, idx);
-                new_rest2 = strncpy(new_rest2, rest+1, rest_size-1);
-                //printf("NEW_BETA: %s|%s\n", new_rest,new_rest2);
-                snprintf(new_rest3, sizeof new_rest3, "%s%s", new_rest, new_rest2);
-                //printf("NEW_REST: %s\n", new_rest3);
-                snprintf(new_prefix, sizeof new_prefix, "%s%s", prefix, &first_char);
-                //printf("NEW_PREF: %s\n", new_prefix);
-                //printf("Prefix: %s\n", prefix);
-                //printf("Rest : %c\n", first_char);
-                //printf("AM i working? %s\n", new_prefix);
-                RecursivePermute( new_prefix, new_rest3, ptr);
+                rest_char = *rest;
+                rest_left = strncpy(rest_left, rest-idx, idx);
+                rest_right = strncpy(rest_right, rest+1, rest_size-1);
+                snprintf(new_rest, sizeof new_rest, "%s%s", rest_left, rest_right);
+                snprintf(new_prefix, sizeof new_prefix, "%s%s", prefix, &rest_char);
+                RecursivePermute( new_prefix, new_rest, ptr);
             }
             rest++;
             idx ++;
         }
     }
 }
-        /*for (i = 0; i < rest_size; i++){
-            /*
-            if (rest.find(rest[i] == i) 
-            {
-                    string newPrefix = prefix + rest[i];
-                    string newRest = rest.substr(0, i) + rest.substr(i+1);
-                    RecursivePermute (newPrefix, newRest, ptr);
-                    }
-                    }
-                   */ 
-
-/*
-int Strlen (char *word){
-    int size = 0;
-    while ( *(word++) != '\0' )
-        size++;
-    return size;
-}*/
