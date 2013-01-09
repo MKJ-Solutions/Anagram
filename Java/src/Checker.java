995a199b142b427741076ea5d917657d0e408562
Kyong-Tae Kim <kyongtae_kim@hotmail.com>
2013-01-09 07:29:45 -0500

    Finished Everything, it still needs to be checked
    added checkFile, checkWordToFile, and removeWords methods

--- Java/src/Checker.java
+++ Java/src/Checker.java
@@ -8,10 +8,12 @@
  * @author Kyong-Tae
  */
 import java.util.*;
+import java.io.*;
         
-public class Checker {
+public class Checker{
     boolean valid = false;
     List<String> validWords = new ArrayList<String>();
+    String locDictionary = "dictionary.txt";
     
     public boolean checkWord(String word){
         if (validWords.contains(word)){
@@ -19,6 +21,44 @@ public class Checker {
             }
             else return false;
         }
+    public boolean checkWordToFile(String word){
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(locDictionary));
+            String dictionaryWord = br.readLine();
+            while(dictionaryWord != null){
+                if (word.equals(dictionaryWord)){
+                    return true;
+                }
+            }
+            br.close();
+        }
+        catch (IOException e){
+            System.out.println("Excetption: " + e.getMessage());
+        }
+        return false;
     }
-    //public void checkFile(String file)
+    
+    public void checkFile(String file){
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(file));
+            String permutedWord = br.readLine();
+            List<String> permutedWords = new ArrayList<String>();
+            while(permutedWord != null){
+                permutedWords.add(permutedWord);
+            }
+            br.close();
+            for(int i = 0; i < permutedWords.size(); i++){
+                if(checkWordToFile(permutedWords.get(i)) == true){
+                    validWords.add(permutedWords.get(i));
+                }
+            }
+        }
+        catch (IOException e){
+            System.out.println("Excetption: " + e.getMessage());
+        }
+}
+    public void removeWords(){
+        validWords.clear();
+    }
+}
     
--- Java/src/Test.java
+++ Java/src/Test.java
@@ -1,8 +1,17 @@
 //import java.util.*;
 class Test{
     public static void main(String[] args){
-        Permutator permute = new Permutator();
-        String letters = "aaab";
-        permute.display(letters);
+        //Permutator permute = new Permutator();
+        //String letters = "aaab";
+        //permute.display(letters);
+        Checker check = new Checker();
+        String word = "abc";
+        String words = "kt.txt"
+        //check.validWords.add(word);
+        if (check.checkWord(word)){
+            System.out.print("haha");
+        }
+        else System.out.print("damn");
+        check.checkFile(words);
     }
 }
