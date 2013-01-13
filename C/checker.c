5f7f6c5f02da3f2d64ebf48cf55010d9b700e01d
Miles Tjandrawidjaja <Miles.Tjandrawidjaja@gmail.com>
2013-01-12 20:52:44 -0500

    Added individual word checking with function isValid

--- C/checker.c
+++ C/checker.c
@@ -24,22 +24,35 @@ typedef int bool;
 unsigned int CheckFile(const char *input_file, const char *output_file, const char *dicitonary_file);
 static size_t LoadFile(const char *faname, size_t max_words, char *word_list[max_words]);
 static void FreeFile(size_t word_count, char *word_list[word_count]);
+bool isValid(const char *word, size_t max_words, char *dictionary[max_words]);
 
 int main(void){
     const char *input_name = "permutations.txt";
     const char *dictionary_name = "dictionary.txt";
     const char *output_name = "words.txt";
-    unsigned int num_words;
-    num_words = CheckFile(input_name, output_name, dictionary_name);
+    unsigned num_words = CheckFile(input_name, output_name, dictionary_name);
     char *valid_words[MAXWORDS] = {0};
-    size_t random = LoadFile(output_name, MAXWORDS, valid_words);
+    size_t word_size = LoadFile(output_name, MAXWORDS, valid_words);
+
+    /* Demonstration on how to use the functions */
     int i;
-    for (i=0; i<random; i++)
+    for (i=0; i<word_size; i++)
     {
-        printf("MyDic: %s\n",valid_words[i]);
+        printf("Word: %s\n",valid_words[i]);
     }
-    printf("Dictionary Size: %d\n", random);
+    printf("Number of Valid Words: %d\n", word_size);
     
+    bool test;
+    const char *rand_word = "miles";
+    test = isValid(rand_word, word_size, valid_words);
+    if (test)
+    {
+        printf("Valid Word\n");
+    }
+    else
+    {
+        printf("Invalid Word\n");
+    }
     return 0;
 }
 
@@ -138,4 +151,24 @@ void FreeFile(size_t word_count, char *word_list[word_count])
 {
     size_t i;
     for (i=0; i<word_count; free(word_list[i]), word_list[i++]=0);
+}
+
+/*
+ * Function: isValid
+ * ==================
+ * Description: Checks if the word is valid against a dictionary
+ */
+bool isValid(const char *word, size_t max_words, char *dictionary[max_words])
+{
+    int i;
+    for (i=0; i<max_words; i++)
+    {
+        if (strcasecmp(word, dictionary[i]) == 0)
+        {
+            printf("Dictionary: %s\n", dictionary[i]);
+            printf("Word: %s\n", word);
+            return True;
+        }
+    }
+    return False;
 }
