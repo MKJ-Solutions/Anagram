d95fe1770261e1f1924035f2875bc1da515624a5
Miles Tjandrawidjaja <Miles.Tjandrawidjaja@gmail.com>
2013-01-12 21:04:42 -0500

    Added notes and documentaion

--- C/stopwatch.c
+++ C/stopwatch.c
@@ -1,3 +1,13 @@
+/*
+ * File: stopwatch.c
+ * Author: Miles Tjandrawidjaja
+ * Last Updated: 01/12/2013
+ * ===============================
+ * Description: This file is use as a stopwatch
+ *
+ * Notes: When compiling use -pthred
+ *        Example gcc stopwatch.c -pthread -o stopwatch
+ */
 #include <time.h>
 #include <stdlib.h>
 #include <stdio.h>
@@ -19,29 +29,33 @@ double getTimer( StopWatch *stopwatch );
 
 main(void){
 
-    CountDown(6);
-    while(1);
+    //CountDown(6);
+    //Hang Program
+    //while(1);
 
-    /*
+    
+    /* Showing what happens when you forget the start the timer and when you actually start the timer */
     StopWatch *stopwatch = InitTimer();
     sleep(1);
     printf( "%g seconds have now passed\n", getTimer(stopwatch));
     StartTimer(stopwatch);
     sleep(3);
     printf( "%g seconds have now passed\n", getTimer(stopwatch));
-    EndTimer(stopwatch);*/
+    EndTimer(stopwatch);
 
-    //clock_t start, end;
-    //double elasped;
-    //Return the current time
-    //time_t current_time= time(NULL);
-    //printf("Current time is %s", ctime(&current_time));
-    //start = clock;
-    //clock(void) return the proccessor time
-    //clock(void)/CLOCKS_PER_SEC gets time in seconds
-    //end = clock();
-    //printf(" Took %ld ticks \n", end-start);
-    //elasped = ((double) (end - start)) / CLOCKS_PER_SEC;
+    /* Forgot why I have the stuff below 
+    clock_t start, end;
+    double elasped;
+    Return the current time
+    time_t current_time= time(NULL);
+    printf("Current time is %s", ctime(&current_time));
+    start = clock;
+    clock(void) return the proccessor time
+    clock(void)/CLOCKS_PER_SEC gets time in seconds
+    end = clock();
+    printf(" Took %ld ticks \n", end-start);
+    elasped = ((double) (end - start)) / CLOCKS_PER_SEC;
+    */
     return 0;
 }
 
@@ -119,6 +133,7 @@ void CountDown(int time_sec){
     int i = 0;
     int *seconds = time_sec;
     pthread_create(&pth,NULL,threadFunc,seconds);
+
     //Below will hold up proccesses
     //pthread_join(pth, NULL /* void ** return value could go here */);
 
